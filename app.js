30f963343e933987eab30de1bff43d023815451a
Jesus Oviedo <joviedo1090@gmail.com>
2023-11-04 17:50:35 -0500

    Adjusted vercel.json

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 const db = mysql.createConnection({
-    host: 'ep-autumn-cherry-62655011-pooler.us-east-1.postgres.vercel-storage.com',
-    user: 'default',
-    database: 'verceldb',
-    password: 'G2kLVBuTp5xU'
+    host: 'viaduct.proxy.rlwy.net',
+    user: 'root',
+    database: 'railway',
+    password: 'ABf46e--fBcAb2bhaCEfcDGa612gfD64'
 });
 
 db.connect((err) => {
